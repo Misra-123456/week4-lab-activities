4afd7dc224fec89250188d29c5e4bb0baa3deb14
Misra-123456 <misrashaikh1234@gmail.com>
2025-02-09 10:43:29 +0200

    file submitted

---
+++ week4-lab5p2/src/Footer.jsx
@@ -0,0 +1,4 @@
+<div class="footer">
+  <p>Copyright © 2024 My JSX App</p>
+  <p>Design</p>
+</div>
---
+++ week4-lab5p2/src/Header.jsx
@@ -0,0 +1,4 @@
+<div class="header">
+  <h1>My JSX App Header</h1>
+  <p>My JSX App sub Header</p>
+</div>
